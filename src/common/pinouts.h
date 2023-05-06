90904669654c302f9cf75f6348ae3fb971960052
lenny <lennyweda@gmail.com>
2023-05-06 13:23:19 +0300

    (f/pubsubclient): Change processor and pinout

--- src/common/pinouts.h
+++ src/common/pinouts.h
@@ -3,6 +3,6 @@
 
 #define SYSTEM_GSM_TX_PIN                   PA2
 #define SYSTEM_GSM_RX_PIN                   PA3
-#define SYSTEM_GSM_POWER_KEY                PB12
+#define SYSTEM_GSM_POWER_KEY                PB1
 #define SYSTEM_SERIAL_DEBUG_RX              PA10
 #define SYSTEM_SERIAL_DEBUG_TX              PA9
