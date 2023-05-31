7fd0ee3b26193fe9afc2b53e4acbbfed5f342b4d
lenny <lennyweda@gmail.com>
2023-05-31 06:41:39 +0300

    (f/state-machine): all pinouts defined

--- src/common/pinouts.h
+++ src/common/pinouts.h
@@ -19,3 +19,16 @@
 #define SYSTEM_QUANTITY_5                   PB3
 #define SYSTEM_QUANTITY_6                   PB4
 #define SYSTEM_DOORSWITCH                   PB5
+#define SYSTEM_TAP_1                        PB7
+#define SYSTEM_TAP_2                        PC9
+#define SYSTEM_TAP_3                        PA11
+#define SYSTEM_TAP_4                        PA13
+#define SYSTEM_SOLENOID_VALVE_1             PC6
+#define SYSTEM_SOLENOID_VALVE_2             PC8
+#define SYSTEM_SOLENOID_VALVE_3             PA8
+#define SYSTEM_SOLENOID_VALVE_4             PA12
+#define SYSTEM_SOLENOID_INTERRUPT_1         PB12
+#define SYSTEM_SOLENOID_INTERRUPT_2         PB13
+#define SYSTEM_SOLENOID_INTERRUPT_3         PB14
+#define SYSTEM_SOLENOID_INTERRUPT_4         PB15
+#define SYSTEM_BUZZER                       PC5
