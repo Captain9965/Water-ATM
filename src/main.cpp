1d3ee4359f22a689f7143451e431fc082ae0e19f
lenny <lennyweda@gmail.com>
2023-06-03 21:04:58 +0300

    (f/rfid_test): minimal rfid test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 
 #include "main.h"
+#include <SPI.h>
+#include "MFRC522.h"
+
+MFRC522 mfrc522(SYSTEM_RFID_SS_PIN, SYSTEM_RFID_RESET_PIN);
+
+long long debug_time = 0;
 
 
 void setup() {
@@ -12,22 +18,40 @@ void setup() {
         /* maybe reset mcu??*/
       }
     }
-    VMC * vmc = VMC::get_default_instance();
-
-    /* user interface: */
-    UI * user_interface = UI::get_default_instance();
-    user_interface->set_page(splashScreenPage::get_default_instance());
-
-    vmc->set_state(vmc_booting::get_default_instance());
+   
     DEBUG_INFO_LN("*****************Starting Application***************");
+    SPI.begin();			// Init SPI bus
+	  mfrc522.PCD_Init();
 
-    vmc->start();
-    user_interface->init();
-    
-    vTaskStartScheduler();
+    pinMode(SYSTEM_RFID_SS_PIN, OUTPUT);
+    digitalWrite(SYSTEM_RFID_SS_PIN, HIGH);
+
+    delay(4);
+    mfrc522.PCD_DumpVersionToSerial();
+  
 }
 
+void check_rfid()
+{
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+	if ( ! mfrc522.PICC_IsNewCardPresent()) {
+		return;
+	}
+
+	// Select one of the cards
+	if ( ! mfrc522.PICC_ReadCardSerial()) {
+		return;
+	}
+
+	// Dump debug info about the card; PICC_HaltA() is automatically called
+	mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+
+}
 void loop() {
-    /* empty */
+  check_rfid();
+  if (millis() - debug_time > 2000){
+    DEBUG_INFO_LN("Scanning");
+    debug_time = millis();
+  }
 }
 
