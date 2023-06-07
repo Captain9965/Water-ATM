b7dafc01c280e09dc600323a64b8446add37a587
lenny <lennyweda@gmail.com>
2023-06-07 06:17:39 +0300

    (f/storage): added database on sd library

--- src/storage/storage.cpp
+++ src/storage/storage.cpp
@@ -15,9 +15,46 @@ storage * storage::get_default_instance(){
     return & SD_store;
 }
 
+void storage::printStorageContent(){
+    assert(settingsTable);
+    settingsTable->printTable();
+}
+
 bool storage::init(){
-    if (!SD.begin(SYSTEM_SD_CS_PIN)){
+
+    if(!settingsTable){
+        settingsTable = new MyTable("machine_settings.csv");
+        settingsTable->printSDstatus();
+        if (!settingsTable->begun()){
+            return false;
+        }
+        /* 7 rows and 2 columns: */
+        settingsTable->begin(7, 2);
+        /* settings map
+            0       1
+        0   Key     value
+        1   Tap1q1  20
+        2   Tap1q2  30
+        3   Tap1q3  40
+        4   Tap1q4  50
+        5   Tap1q5  60
+        6   Tap1q6  70   
+        */
+       
+    }
+    if (settingsTable->countCols() == 0 && settingsTable->countRows() == 0){
+        DEBUG_INFO_LN("Writing Default values....");
+        settingsTable->writeCell(0, 0, "KEY"); settingsTable->writeCell(0, 1, "VALUE");
+        settingsTable->writeCell(1, 0, "Tap1q1"); settingsTable->writeCell(1, 1, "20");
+        settingsTable->writeCell(2, 0, "Tap1q2"); settingsTable->writeCell(2, 1, "30");
+        settingsTable->writeCell(3, 0, "Tap1q3"); settingsTable->writeCell(3, 1, "40");
+        settingsTable->writeCell(4, 0, "Tap1q4"); settingsTable->writeCell(4, 1, "50");
+        settingsTable->writeCell(5, 0, "Tap1q5"); settingsTable->writeCell(5, 1, "60");
+        settingsTable->writeCell(6, 0, "Tap1q6"); settingsTable->writeCell(6, 1, "70");
+    }
+    if (settingsTable->countCols() == 0 && settingsTable->countRows() == 0){
         return false;
     }
     return true;
+    
 }
