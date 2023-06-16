a1c873f1d94302dd54e2dc27f8f26eeca96cafcd
lenny <lennyweda@gmail.com>
2023-06-16 17:14:25 +0300

    (f/settings_storage): use tag to test EEPROM and SD card storage

--- src/vmc/idle/vmc_idle.cpp
+++ src/vmc/idle/vmc_idle.cpp
@@ -35,7 +35,11 @@ int vmc_idle::run(){
       DEBUG_INFO(_time.hour);
       DEBUG_INFO(":");
       DEBUG_INFO_LN(_time.minutes);
-      storage::get_default_instance()->printStorageContent();
+      storage::get_default_instance()->printSDCardContent();
+      double tap1_quantity_1 = 0.0;
+      EEPROM.get(TAP1_QUANTITY_1_ADDRESS, tap1_quantity_1);
+      
+      DEBUG_INFO("Tap 1 quantity 1 is "); DEBUG_INFO_LN(String(tap1_quantity_1));
       #ifdef MEM_DEBUG
       stack_debug();
       #endif
@@ -59,8 +63,13 @@ void vmc_idle::run_sensors(){
     String read_string = RFID::get_default_instance()->read_uid();
     if(isServiceTag(read_string)){
 
-        DEBUG_INFO_LN("Service Tag detected");
-
+        DEBUG_INFO_LN("Incrementing Admin Cash..");
+        uint32_t adminCash = 0;
+        storage::get_default_instance()->getAdminCash(&adminCash);
+        storage::get_default_instance()->setAdminCash(++adminCash);
+        double tap1_quantity_1 = 0.0;
+        EEPROM.get(TAP1_QUANTITY_1_ADDRESS, tap1_quantity_1);
+        EEPROM.put(TAP1_QUANTITY_1_ADDRESS, ++tap1_quantity_1);
     }
 
     /* time: */
