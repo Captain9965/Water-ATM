2ab0413ff44983163fb78587a1d722f6a7322ea2
lenny <lennyweda@gmail.com>
2023-06-30 18:25:51 +0300

    (f/booting_logig): booting state logic

--- src/ui/booting_page/booting_page.cpp
+++ src/ui/booting_page/booting_page.cpp
@@ -1,6 +1,9 @@
 #include "booting_page.h"
 #include "vmc/vmc_flags.h"
 #include "ui/ui_input.h"
+#include "storage/storage.h"
+#include "../error_page/error_page.h"
+#include "../idle_page/idle_page.h"
 
 
 
@@ -11,7 +14,6 @@ bootingPage::bootingPage(){
 
 int bootingPage::load(){
    screen_boot_timer = millis();
-   net_check_timer = millis();
     return 0;
 }
 
@@ -20,52 +22,32 @@ int bootingPage::update(){
         load();
         loaded = true;
     }
+    get_display1()->setCursor(1, 1);
+    get_display1()->print("Booting...");
 
-    if ((millis() - net_check_timer) >= BOOT_NET_CHECK_TIMER_INTERVAL){
-        check_vmc_flag(VMC_NET_CONNECTED) ? display_net_connected() : display_net_notConnected();
-        net_check_timer = millis();
-    }
-    if(uiInput::get_default_instance()->joystick_button_pressed()){
-        DEBUG_INFO_LN("Button press detected!!");
-    }
-    if (uiInput::get_default_instance()->joystick_up()){
-        DEBUG_INFO_LN("Joystick up!!");
-    }
-    if (uiInput::get_default_instance()->joystick_down()){
-        DEBUG_INFO_LN("Joystick down!!");
-    }
-    if (uiInput::get_default_instance()->joystick_left()){
-        DEBUG_INFO_LN("Joystick left!!");
-    }
-    if (uiInput::get_default_instance()->joystick_right()){
-        DEBUG_INFO_LN("Joystick right!!");
-    }
-    input_flags_t tap;
-    if(uiInput::get_default_instance()->tap_button_pressed(tap)){
-        get_display1()->clear();
-        get_display1()->setCursor(2, 2);
-        get_display1()->print("Tap ");
-        get_display1()->setCursor(2, 3);
-        get_display1()->print(tap);
-        DEBUG_INFO_LN("Button pressed");
+    //: Get booting info:
+    if (check_vmc_flag(VMC_MEMORY_ERROR)){
+        clear_vmc_flag(VMC_MEMORY_ERROR);
+        this->ui->set_page(errorPage::get_default_instance("MEMORY ERROR"));
+        return -1;
     }
 
-    input_flags_t quantity;
-    if(uiInput::get_default_instance()->quantity_button_pressed(quantity)){
-        get_display1()->clear();
-        get_display1()->setCursor(2, 2);
-        get_display1()->print("Quantity ");
-        get_display1()->setCursor(2, 3);
-        get_display1()->print(quantity);
-        DEBUG_INFO_LN("Button pressed");
+    if (check_vmc_flag(VMC_STORAGE_ERROR)){
+        clear_vmc_flag(VMC_STORAGE_ERROR);
+        this->ui->set_page(errorPage::get_default_instance("SD CARD ERROR"));
+        return -1;
     }
 
-    if(!uiInput::get_default_instance()->door_closed()){
-        get_display1()->clear();
-        get_display1()->setCursor(2, 2);
-        get_display1()->print("Door Open");
+     if (check_vmc_flag(VMC_BOOTING_DONE)){
+        clear_vmc_flag(VMC_BOOTING_DONE);
+        stop();
     }
-   
+
+    return 0;
+}
+
+int bootingPage::stop(){
+    this->ui->set_page(idlePage::get_default_instance());
     return 0;
 }
 
