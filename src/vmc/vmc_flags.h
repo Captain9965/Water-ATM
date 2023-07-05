2df3ed23ebe99ee3c670e05ea168239f4be7dca2
lenny <lennyweda@gmail.com>
2023-07-05 07:19:18 +0300

    (f/dispense_logic): additional dispense flags

--- src/vmc/vmc_flags.h
+++ src/vmc/vmc_flags.h
@@ -7,7 +7,11 @@ typedef enum vmc_flags{
     VMC_SD_CARD_OK = 1 << 1,
     VMC_MEMORY_ERROR = 1 << 2,
     VMC_STORAGE_ERROR = 1 << 3,
-    VMC_BOOTING_DONE = 1 << 4
+    VMC_BOOTING_DONE = 1 << 4, 
+    VMC_DISPENSE_TAP1 = 1 << 5, 
+    VMC_DISPENSE_TAP2 = 1 << 6,
+    VMC_DISPENSE_TAP3 = 1 << 7,
+    VMC_DISPENSE_TAP4 = 1 << 8
 
 
 }vmc_flags_t;
