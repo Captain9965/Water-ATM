6d22f2e8516fd14f12acdeec7229e8a7c443e817
lenny <lennyweda@gmail.com>
2023-07-29 21:49:02 +0300

    (f/dispensing_logic): additions to the dispensing page

--- src/ui/dispensing_page/dispensing_page.h
+++ src/ui/dispensing_page/dispensing_page.h
@@ -1,12 +1,21 @@
 #pragma once
 #include "ui/ui.h"
+#include "vmc/vmc_flags.h"
+#include "services/dispense_system.h"
 
 class dispensingPage: public Page{
     public:
-        dispensingPage();
+        dispensingPage(vmc_flags_t initialTap);
         int load() override;
         int update() override;
-        static dispensingPage* get_default_instance();
+        int stop();
+        static dispensingPage* get_default_instance(vmc_flags_t initialTap);
     private:
-        void display_button_pressed_for_dispense();
+        void clear_amount();
+        void check_for_quantity_selection();
+        void update_dispense_quantities();
+        void display_button_pressed_for_dispense(vmc_flags_t & tap);
+        vmc_flags_t  _initialTap;
+        DispenseSystem * _dispense_instance = nullptr;
+        float amount1, amount2, amount3, amount4;
 };
