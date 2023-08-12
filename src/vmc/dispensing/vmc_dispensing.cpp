5095854c0ef8ddec1b5d2cd1756fe053a170c045
lenny <lennyweda@gmail.com>
2023-08-12 23:13:46 +0300

    (f/dispensing_logic): instantiate and run dispense group

--- src/vmc/dispensing/vmc_dispensing.cpp
+++ src/vmc/dispensing/vmc_dispensing.cpp
@@ -8,8 +8,6 @@ vmc_dispensing::vmc_dispensing(){
 int vmc_dispensing::start(){
     started = true;
     DEBUG_INFO_LN("Dispensing state starting..");
-
-    
     return 0;
 }
 
@@ -31,12 +29,17 @@ int vmc_dispensing::run(){
     /* check whether all dispensing systems are done and go back to idle state: */
     if(!_dispense_group){
         // create instance then start
+        DEBUG_INFO_LN("Begin dispensing instance.......");
         _dispense_group = dispenseGroup::get_default_instance();
         _dispense_group->add(_initialTap);
     }
-    _dispense_group->run();
+    _next_dispense_instance = _dispense_group->run();
+    if(!_next_dispense_instance){
+        DEBUG_INFO_LN("Next dispense instance is null....");
+    }
     if (_dispense_group->is_empty()){
         _dispense_group = nullptr;
+        set_vmc_flag(VMC_DISPENSE_DONE);
         stop();
     }
     return 0;
@@ -44,6 +47,7 @@ int vmc_dispensing::run(){
 
 int vmc_dispensing::stop(){
     /* stop and return to idle state: */
+    _next_dispense_instance = nullptr;
     DEBUG_INFO_LN("Dispensing state stopping...");
     this->vmc->set_state(vmc_idle::get_default_instance());
     return 0;
@@ -54,9 +58,9 @@ vmc_dispensing* vmc_dispensing::get_default_instance(){
     return &vmc_state;
 }
 
-dispenseGroup * vmc_dispensing::get_dispense_group(){
-    return _dispense_group;
-}
+DispenseSystem * vmc_dispensing::get_current_dispense_instance(){
+    return _next_dispense_instance;
+} 
 
 void vmc_dispensing::run_sensors(){
   
