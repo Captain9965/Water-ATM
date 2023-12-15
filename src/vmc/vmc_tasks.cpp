ecc473c449c54a2dbd2110f904d6e656e4d7caa3
lenny <lennyweda@gmail.com>
2023-12-15 07:41:05 +0300

    (f/dispensing_logic): more stable perfomance

--- src/vmc/vmc_tasks.cpp
+++ src/vmc/vmc_tasks.cpp
@@ -6,17 +6,17 @@
 
 /* task handles: */
 BaseType_t * get_comms_taskhandle(){
-    static BaseType_t comms_taskhandle = xTaskCreate(comms_task, "comms_task", 500, nullptr, 0, nullptr);
+    static BaseType_t comms_taskhandle = xTaskCreate(comms_task, "comms_task", 500, nullptr, configMAX_PRIORITIES - 3, nullptr);
     return & comms_taskhandle;
 }
 
 BaseType_t * get_main_taskhandle(){
-    static BaseType_t main_taskhandle = xTaskCreate(main_task, "main_task", 500, nullptr, 0, nullptr);
+    static BaseType_t main_taskhandle = xTaskCreate(main_task, "main_task", 500, nullptr, configMAX_PRIORITIES - 1, nullptr);
     return & main_taskhandle;
 }
 
 BaseType_t * get_input_task(){
-  static BaseType_t input_taskhandle = xTaskCreate(input_task, "input_task", 100, nullptr, 1, nullptr );
+  static BaseType_t input_taskhandle = xTaskCreate(input_task, "input_task", 100, nullptr, configMAX_PRIORITIES - 2, nullptr );
   return & input_taskhandle;
 }
 
@@ -31,7 +31,7 @@ void main_task(void * pvParameters){
   while(1){
     VMC::get_default_instance()->run();
     UI::get_default_instance()->update();
-    wait_ms(20);
+    wait_ms(50);
   } 
 }
 
@@ -40,6 +40,6 @@ void input_task(void * pvParameters){
   while (1){
     input_instance->update_joystick_state();
     input_instance->update_push_button_states();
-    wait_ms(10);
+    wait_ms(25);
   }
 }
